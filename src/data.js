bdb068c6dcedeb1b1cadf367c04fdb4720d22b33
imooclam <dragon158man@gmail.com>
2023-04-24 22:44:03 +0200

    HTTP

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${
+export const API_ENDPOINT = `http://www.omdbapi.com/?apikey=${
   import.meta.env.MOVIE_API_KEY
 }`;
 console.log(API_ENDPOINT);
